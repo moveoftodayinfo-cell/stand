a21d707ba13b8ecf27585035923f3c594eb57e2c
moveoftodayinfo-cell <moveoftoday.info@gmail.com>
2026-02-01 20:08:52 +0900

    chore: 버전 1.0.72 (versionCode 72) 릴리즈

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.moveoftoday.walkorwait"
         minSdk = 26
         targetSdk = 36
-        versionCode = 71
-        versionName = "1.0.71"
+        versionCode = 72
+        versionName = "1.0.72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
